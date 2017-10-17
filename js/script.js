6b595d9a40bba1da48753a13bf6b57a9df6f0a7f
YuliaNikonorova <ylia.nikonorova@yandex.ru>
2017-10-17 23:50:44 +0300

    17.10.2017

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/*Всплывающее окно авторизазия*/
+function entrance(){
+	var a=document.querySelector(".autorisation");
+	a.style.display="block";
+}
+/*Закрытие окна авторизазия*/
+function close_autorisation_times(){
+	var a=document.querySelector(".autorisation");
+	a.style.display="none";
+}
+/*Всплывающее окно напишите нам*/
+function entrance_window_pochta(){
+	var a=document.querySelector(".window_pochta");
+	a.style.display="block";
+}
+/*Закрытие окна напишите нам*/
+function close_window_pochta(){
+	var a=document.querySelector(".window_pochta");
+	a.style.display="none";
+}
+/*Всплывающее окно карта*/
+function entrance_maps_times(){
+	var a=document.querySelector(".iframe_maps");
+	a.style.display="block";
+}
+/*Закрытие окна карта*/
+function close_iframe_maps(){
+	var a=document.querySelector(".iframe_maps");
+	a.style.display="none";
+}
